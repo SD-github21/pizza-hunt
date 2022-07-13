c12d597fadfd5ad556e402e3b314fd1b2a32cf25
SD-github21 <sapandonde.gmail.com>
2022-07-13 00:11:46 -0500

    added code to add and delete replies about comments

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -20,9 +20,26 @@ const commentController = {
             res.json(dbPizzaData);
         })
         .catch(err => res.json(err));
-
     },
      
+    // add a reply
+    addReply({ params, body }, res) {
+        console.log(body);
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+          .then(dbPizzaData => {
+            if(!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));   
+    },
+
 
     // remove comment
     removeComment({ params }, res) {
@@ -44,6 +61,20 @@ const commentController = {
             }
             res.json(dbPizzaData);
           })
+          .catch(err => res.json(err));
+    },
+
+
+    // remove reply
+
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId} } },
+            { new: true }
+          )
+          .then(dbPizzaData => 
+            res.json(dbPizzaData))
           .catch(err => res.json(err));
     }
 };
